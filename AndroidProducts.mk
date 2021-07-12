215241060977f7413aad9f47aa7e6c2482d1c904
ANKIT KENE <keneankit01@gmail.com>
2021-07-12 17:01:27 +0530

    ADD EVOULTION X

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_PL2.mk
+    $(LOCAL_DIR)/aosp_PL2.mk \
+    $(LOCAL_DIR)/evolution_PL2.mk \
 
 COMMON_LUNCH_CHOICES := \
     aosp_PL2-eng \
     aosp_PL2-user \
-    aosp_PL2-userdebug
-
+    aosp_PL2-userdebug \
+    evolution_PL2-eng \
+    evolution_PL2-user \
+    evolution_PL2-userdebug
